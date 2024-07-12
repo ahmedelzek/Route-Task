be926932a679be76d3801116d17f506bdfa89157
Ahmed <elzek989@gmail.com>
2024-07-12 17:08:43 +0300

    Add ProductAdapter and ProductsViewModel

---
+++ app/src/main/java/com/example/route/route_task/base/FragmentExtension.kt
@@ -0,0 +1,45 @@
+package com.example.route.route_task.base
+
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+
+fun Fragment.showDialog(
+    title: String,
+    message: String,
+    posActionName: String? = null,
+    posActionCallBack: (() -> Unit)? = null,
+    negActionName: String? = null,
+    negActionCallBack: (() -> Unit)? = null,
+    isCancelable: Boolean = true,
+): AlertDialog {
+    val alertDialogBuilder = AlertDialog.Builder(requireContext())
+    alertDialogBuilder.setMessage(message)
+
+    alertDialogBuilder.setPositiveButton(
+        posActionName,
+    ) { dialog, _ ->
+        dialog?.dismiss()
+        posActionCallBack?.invoke()
+    }
+    alertDialogBuilder.setNegativeButton(
+        negActionName,
+    ) { dialog, _ ->
+        dialog?.dismiss()
+        negActionCallBack?.invoke()
+    }
+
+    alertDialogBuilder.setCancelable(isCancelable)
+    return alertDialogBuilder.show()
+}
+
+fun Fragment.showDialog(viewMessage: ViewMessage): AlertDialog {
+    return showDialog(
+        title = viewMessage.title,
+        message = viewMessage.message,
+        posActionName = viewMessage.posActionName,
+        posActionCallBack = viewMessage.posAction,
+        negActionName = viewMessage.negActionName,
+        negActionCallBack = viewMessage.negAction,
+        isCancelable = viewMessage.isDismissible,
+    )
+}
---
+++ app/src/main/java/com/example/route/route_task/ui/product/fragment/ProductsViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.route.route_task.ui.product.fragment
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.route.domain.models.Product
+import com.example.route.domain.usecase.GetProductUseCase
+import com.example.route.route_task.base.ViewMessage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class ProductsViewModel @Inject
+constructor(
+    private val getProductUseCase: GetProductUseCase
+) : ViewModel() {
+    var loadingLiveData = MutableLiveData<Boolean>()
+    var errorMessage = MutableLiveData<ViewMessage>()
+    var prodcutListLiveData = MutableLiveData<List<Product>>()
+    fun getAllProducts() {
+        try {
+            loadingLiveData.value = true
+            viewModelScope.launch {
+                val listOfProduct = getProductUseCase.getALlProduct()
+                prodcutListLiveData.postValue(listOfProduct)
+                loadingLiveData.value = false
+            }
+        } catch (e: Exception) {
+            loadingLiveData.value = false
+            errorMessage.value = ViewMessage(
+                title = "Error",
+                message = e.localizedMessage,
+            )
+        }
+    }
+}
