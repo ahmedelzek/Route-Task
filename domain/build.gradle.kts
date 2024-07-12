3bed4d229450b8808a599c03b9c4ea9d44da0beb
Ahmed <elzek989@gmail.com>
2024-07-12 14:20:20 +0300

    Prepare Dependencies And Layers Files

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.kotlin.parcelize)
@@ -7,17 +7,14 @@ plugins {
 }
 
 android {
-    namespace = "com.example.route.route_task"
+    namespace = "com.example.route.domain"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.route.route_task"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -43,26 +40,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(project(":data"))
-    implementation(project(":domain"))
-
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.fragment.ktx)
-    testImplementation(libs.kotlinx.coroutines.test)
-    // hilt
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
-    // shimmer effect
-    implementation(libs.shimmer.effect)
-    // rounded image
-    implementation(libs.roundedimage)
-    // picasso
-    implementation(libs.picasso)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "Route_Task"
 include(":app")
- 
+include(":domain")
+include(":data")
